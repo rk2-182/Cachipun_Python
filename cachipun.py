d97462fbd1070b6adf1df4e6428f3ea198b155c9
rk2-182 <ricardoplus@live.cl>
2021-07-12 12:59:36 -0400

    parte 6 JuegoCachipun PO se agrega audio

--- cachipun.py
+++ cachipun.py
@@ -1,15 +1,30 @@
-import random,time,os
+"""https://pypi.org/project/playsound/"""
+
+import random
+import time
+import os
+#from playsound import playsound
+import pygame
 
 class Cachipun:
 
     tijera = chr(9996)
     papel = chr(9995)
     piedra = chr(9994)
+    puntosH = 0
+    puntosM = 0
+    pygame.mixer.init()
+    #cancion =playsound('Boss-Fight.mp3')
 
-    #Metodos
+    """
+    def __init__(self,cancion):
+        self.cancion = playsound(cancion)
+        #self.cancion =playsound('Boss-Fight.mp3')
+    """
+    # Metodos
     def maquina(self):
-        #generar 'jugada' aleatoria
-        self.__jugada = random.randrange(0,3) #atributo privado
+        # generar 'jugada' aleatoria
+        self.__jugada = random.randrange(0, 3)  # atributo privado
         if self.__jugada == 0:
             return 'piedra'
         if self.__jugada == 1:
@@ -17,21 +32,20 @@ class Cachipun:
         if self.__jugada == 2:
             return 'tijera'
 
-
-
     def presentacion(self):
         os.system('cls')
         print("*********Bienvenido al juego Cachipun Maquina Vs Humano*********")
         print("Opciones: ")
-       
-        print("tijera = ",self.tijera)
-        print("papel = ",self.papel)
-        print("piedra = ",self.piedra)
 
+        print("1.-tijera = ", self.tijera)
+        print("2.-piedra = ", self.piedra)
+        print("3.-papel = ", self.papel)
+        
 
     def humano(self):
-       
-        self.opcionH = int(input("\nIngrese su opcion: (1={},2={},3={})".format(self.tijera,self.piedra,self.papel)))
+        time.sleep(1)
+        self.opcionH = int(input("\nIngrese su opcion: (1={},2={},3={})".format(
+            self.tijera, self.piedra, self.papel)))
 
         if self.opcionH == 1:
             return 'tijera'
@@ -40,83 +54,100 @@ class Cachipun:
         if self.opcionH == 3:
             return 'papel'
 
-    def jugar(self):
+    def jugar(self,cancion):
+        pygame.mixer.music.load(cancion)
+        pygame.mixer.music.play(loops=0)
         repeticiones = 0
-        puntosH = 0
-        puntosM = 0
+        self.puntosH = 0
+        self.puntosM = 0
 
-        #0 reps es menor a 3? si es asi entrar en ciclo
+        # 0 reps es menor a 3? si es asi entrar en ciclo
         while repeticiones < 3:
-            #guardar en una variable la opcion humana ingresada
+            
+            # guardar en una variable la opcion humana ingresada
+            # ejecutamos la funcion humano la cual pedira ingresar por teclado una opcion
             resultadoHumano = self.humano()
 
-            #guardar en una variable el resultado aleatorio de la maquina
-            resultadoMaquina=self.maquina()
+            # guardar en una variable el resultado aleatorio de la maquina
+            # se ejecuta la opcion maquina y esta aleatoriamente generara su opcion
+            resultadoMaquina = self.maquina()
 
-            print("Maquina:{}".format(resultadoMaquina))
             print("Humano: {}".format(resultadoHumano))
-            time.sleep(1)
-            #*****************buscar ganador****************************
-            #Piedra:
-            #Empate humano
+            print("Maquina:{}".format(resultadoMaquina))
+
+            # *****************buscar ganador****************************
+            # Piedra:
+            # Empate humano
             if resultadoHumano == 'piedra' and resultadoMaquina == 'piedra':
-                puntosH +=1
-                puntosM +=1
+                self.puntosH += 1
+                self.puntosM += 1
                 print("Piedra vs Piedra Empate!")
-            #Pierde humano
+            # Pierde humano
             elif resultadoHumano == 'piedra' and resultadoMaquina == 'papel':
-                puntosH+=0
-                puntosM+=1
+                self.puntosH += 0
+                self.puntosM += 1
                 print("Piedra vs Papel Gana Papel")
-            #Gana humano
+            # Gana humano
             elif resultadoHumano == 'piedra' and resultadoMaquina == 'tijera':
-                puntosH+=1
-                puntosM+=0
+                self.puntosH += 1
+                self.puntosM += 0
                 print("Piedra vs Tijera Gana Piedra!")
-            
-            #Papel:
-            #Gana humano
+
+            # Papel:
+            # Gana humano
             if resultadoHumano == 'papel' and resultadoMaquina == 'piedra':
-                puntosH +=1
-                puntosM +=0
-            #Empate humano
+                self.puntosH += 1
+                self.puntosM += 0
+                print("Papel vs Piedra gana Papel!")
+            # Empate humano
             elif resultadoHumano == 'papel' and resultadoMaquina == 'papel':
-                puntosH+=1
-                puntosM+=1
-            #Gana maquina
+                self.puntosH += 1
+                self.puntosM += 1
+                print("Papel vs Papel Empate!")
+            # Gana maquina
             elif resultadoHumano == 'papel' and resultadoMaquina == 'tijera':
-                puntosH+=0
-                puntosM+=1
-            
-            #Tijera:
-            #Pierde humano
+                self.puntosH += 0
+                self.puntosM += 1
+                print("Papel vs tijera gana Tijera!")
+            # Tijera:
+            # Pierde humano
             if resultadoHumano == 'tijera' and resultadoMaquina == 'piedra':
-                puntosH +=0
-                puntosM +=1
-            #Gana humano
+                self.puntosH += 0
+                self.puntosM += 1
+                print("Tijera vs Piedra gana Piedra!")
+            # Gana humano
             elif resultadoHumano == 'tijera' and resultadoMaquina == 'papel':
-                puntosH+=1
-                puntosM+=0
-            #Empate maquina
+                self.puntosH += 1
+                self.puntosM += 0
+                print("Tijera vs papel gana Tijera!")
+            # Empate maquina
             elif resultadoHumano == 'tijera' and resultadoMaquina == 'tijera':
-                puntosH+=1
-                puntosM+=1
-            
+                self.puntosH += 1
+                self.puntosM += 1
+                print("Tijera vs tijera Empate!")
 
-            repeticiones+=1
+            repeticiones += 1
 
+            time.sleep(1)
             print("\nResultado puntos: ")
             print("******************************")
-            print("Maquina: {}".format(puntosM))
-            print("Humano: {}".format(puntosH))
+            print("Humano: {}".format(self.puntosH))
+            print("Maquina: {}".format(self.puntosM))
 
+    def ganador(self):
+        if self.puntosH > self.puntosM:
+            print("Enhorabuena Humano haz ganado!")
+        elif self.puntosH < self.puntosM:
+            print("Maquina a ganado, Jajaja >:)")
+        else:
+            print("Han Empatado!")
+            
         
-#=========Crear objeto de la clase cachipun=========
+# =========Crear objeto de la clase cachipun=========
 maquina = Cachipun()
-
-
 maquina.presentacion()
-maquina.jugar()
+maquina.jugar('Boss-Fight.mp3')
+maquina.ganador()
 
 print("Fin")
 
